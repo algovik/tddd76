02323e0182b246b055a537f539e8cd31e6e6e798
Lisa <Lisa@Elisabeths-MacBook-Pro.local>
2014-09-05 11:35:41 +0200

    Labb 4 kommenterad och städad

--- Lab1-4/lab1-4.cc
+++ Lab1-4/lab1-4.cc
@@ -2,60 +2,17 @@
 #include <vector>
 using namespace std;
 
+/**
+	A struct for being able to keep track of the word entered
+	and how many times it has been entered
+*/
 struct word_entry{  
     int count;  
 	string word;
 };
 
-bool cmp(const word_entry& lw, const word_entry& rw){
-	return lw.word < rw.word;
-} 
-
-vector<word_entry> insert(string s, vector<word_entry> wordbook){
-
-	word_entry temp;
-	temp.word = s;
-	temp.count = 1;
-
-	// Sätter in på rätt plats direkt
-	bool inserted = false;
-	for(vector<word_entry>::iterator it = wordbook.begin(); it != wordbook.end(); ++it){
-		if(s.compare(it->word) < 0){
-			wordbook.insert(it, temp);
-			inserted = true;
-			break;
-		}
-	}
-	if(!inserted){
-		wordbook.push_back(temp);
-	}
-
-	// Sätter in och sorterar sen
-	// wordbook.push_back(temp);
-	// sort(wordbook.begin(), wordbook.end(), cmp);
-	return wordbook;
-}
-
-void print(vector<word_entry> wordbook){
-	cout << "\t Ord \t Antal" << endl << endl;
-	
-	//Tydligen mer korrekt för C++
-	for(vector<word_entry>::iterator it = wordbook.begin(); it != wordbook.end(); ++it){
-		cout << "\t" << it->word << "\t" << it->count << endl;
-	}
-
-	//C++11
-	// for(auto &i : wordbook){
-	// 	cout << "\t" << i.word << "\t" << i.count << endl; 
-	// }
-
-	//Funkar
-	// for(int i = 0; i < wordbook.size(); ++i) {
-	// 	cout <<  "\t" << wordbook[i].word <<  "\t" << wordbook[i].count << endl; 
-	// }
-}
-
-
+vector<word_entry> insert(string s, vector<word_entry> wordbook);
+void print(vector<word_entry> wordbook);
 
 int main(){
 	vector<word_entry> wordbook;
@@ -83,4 +40,43 @@ int main(){
 	}
 	print(wordbook);
 	return 0;
+}
+
+/**
+	Creates a word_entry struct for the word and put it in the right alphabetic position 
+	in the wordbook 	
+	@param s the word entered, wordbook the vector containing all the words
+	@return the wordbook with a new word entered	 
+ */
+vector<word_entry> insert(string s, vector<word_entry> wordbook){
+
+	word_entry temp;
+	temp.word = s;
+	temp.count = 1;
+
+	bool inserted = false;
+	for(vector<word_entry>::iterator it = wordbook.begin(); it != wordbook.end(); ++it){
+		if(s.compare(it->word) < 0){
+			wordbook.insert(it, temp);
+			inserted = true;
+			break;
+		}
+	}
+	if(!inserted){
+		wordbook.push_back(temp);
+	}
+
+	return wordbook;
+}
+
+/**
+	Print all the words in the wordbooks and how many time they appeared 
+	@param wordbook the vector containing all the words 
+ */
+void print(vector<word_entry> wordbook){
+	cout << "\t Ord \t Antal" << endl << endl;
+	
+	for(vector<word_entry>::iterator it = wordbook.begin(); it != wordbook.end(); ++it){
+		cout << "\t" << it->word << "\t" << it->count << endl;
+	}
 }
