7d8de7ee9f8e9225af30591b9cfe20f30dc5afbc
algovik <algovik@gmail.com>
2014-09-16 14:23:01 +0200

    fixed indentation on lab1-5.cc

--- Lab1-5/lab1-5.cc
+++ Lab1-5/lab1-5.cc
@@ -14,75 +14,75 @@ void build_lists(istream in, List list_1, List list_2);
 
 int main(){
 
-   // Kontrollera att ett argument (filnamn) angivits på kommandoraden.
-   // Om inte skriv ut ett felmeddelande och avsluta programmet.
+    // Kontrollera att ett argument (filnamn) angivits på kommandoraden.
+    // Om inte skriv ut ett felmeddelande och avsluta programmet.
 
-   // öppna en infilström för den fil vars namn givits på kommandoraden.
-   // Kontrollera att öppningen lyckas, om inte ska ett felmeddelande
-   // skrivas ut och programmet avslutas.
+    // öppna en infilström för den fil vars namn givits på kommandoraden.
+    // Kontrollera att öppningen lyckas, om inte ska ett felmeddelande
+    // skrivas ut och programmet avslutas.
 
-   // Deklarera två tomma listor, list_1 och list_2
-   list_1 = new List{nullptr};
-   list_2 = new List{nullptr};
-   // Kontrollera med empty() att listorna är tomma och skriv ut det.
-   if(empty(list_1)){
-       cout << "list_1 is empty\n";
-   }
-   if(empty(list_2)){
-       cout << "list_2 is empty\n";
-   }
-   // Anropa build_lists() för att läsa namn från indata och sätta in i namnen
-   // i list_1 och list_2, enligt vad som sagts om build_lists() ovan.
-   build_lists(in,list_1,list_2);
+    // Deklarera två tomma listor, list_1 och list_2
+    List list_1{nullptr};
+    List list_2{nullptr};
+    // Kontrollera med empty() att listorna är tomma och skriv ut det.
+    if(empty(list_1)){
+        cout << "list_1 is empty\n";
+    }
+    if(empty(list_2)){
+        cout << "list_2 is empty\n";
+    }
+    // Anropa build_lists() för att läsa namn från indata och sätta in i namnen
+    // i list_1 och list_2, enligt vad som sagts om build_lists() ovan.
+    build_lists(in,list_1,list_2);
 
-   cout << "Lista 1 efter inläsning av namn:\n";
-   print(list_1,out); // Skriv ut list_1 med print()
-   cout << "Lista 2 efter inläsning av namn:\n";
-   print(list_2,out); // Skriv ut list_2 med print()
-   cout << "Lista 1 utskriven i omvänd ordning:\n";
-   print_reversed(list_1,out); // Skriv ut list_1 med print_reversed()
-   cout << "Lista 1 vänds.\n";
-   // Vänd innehållet i list_1 med reverse();
-   cout << "Lista 1 efter vändning:\n";
-   print(list_1,out); // Skriv ut list_1 med print()
+    cout << "Lista 1 efter inläsning av namn:\n";
+    print(list_1,out); // Skriv ut list_1 med print()
+    cout << "Lista 2 efter inläsning av namn:\n";
+    print(list_2,out); // Skriv ut list_2 med print()
+    cout << "Lista 1 utskriven i omvänd ordning:\n";
+    print_reversed(list_1,out); // Skriv ut list_1 med print_reversed()
+    cout << "Lista 1 vänds.\n";
+    // Vänd innehållet i list_1 med reverse();
+    cout << "Lista 1 efter vändning:\n";
+    print(list_1,out); // Skriv ut list_1 med print()
 
-   cout << "Lista 2 raderas.\n";
-   clear(list_2); // Radera list_2 med clear()
-   if (empty(list_2)){
+    cout << "Lista 2 raderas.\n";
+    clear(list_2); // Radera list_2 med clear()
+    if (empty(list_2)){
         cout << "Lista 2 är tom.\n";
     }else{
         cout << "Lista 2 är inte tom.\n";
     }
-   cout << "Lista 2 tilldelas en kopia av lista 1.\n";
-   // Använd copy() för att kopiera list_1
-   cout << "Lista 2 innehåller:\n";
-   // Skriv ut list_2 med print()
+    cout << "Lista 2 tilldelas en kopia av lista 1.\n";
+    // Använd copy() för att kopiera list_1
+    cout << "Lista 2 innehåller:\n";
+    // Skriv ut list_2 med print()
 
-   cout << "Lista 2 raderas.\n";
-   // Radera list_2 med clear()
-   cout << "Lista 1 och 2 byter innehåller.\n";
-   // Använd swap()
-   // if (empty(list_1))
-   //    cout << "Lista 1 är tom.\n";
-   // else
-   //    cout << "Lista 1 är inte tom.\n";
-   // cout << "Lista 2 innehåller:\n";
-   // Skriv ut list_2 med print()
+    cout << "Lista 2 raderas.\n";
+    // Radera list_2 med clear()
+    cout << "Lista 1 och 2 byter innehåller.\n";
+    // Använd swap()
+    // if (empty(list_1))
+    //    cout << "Lista 1 är tom.\n";
+    // else
+    //    cout << "Lista 1 är inte tom.\n";
+    // cout << "Lista 2 innehåller:\n";
+    // Skriv ut list_2 med print()
 
-   cout << "Lista 2 raderas.\n";
-   // Radera list_2 med clear()
+    cout << "Lista 2 raderas.\n";
+    // Radera list_2 med clear()
 
-   // Kontrollera med empty() att listorna är tomma och skriv ut det.
+    // Kontrollera med empty() att listorna är tomma och skriv ut det.
 
-   cout << "Programmet avslutas.\n";
+    cout << "Programmet avslutas.\n";
 
-   return 0;
-}
-
-
-void build_lists(istream& in, List list_1, List list_2){
-    while(in >> x){
-        append(list_1,x,age?);
-        insert(list_2,x,age?);
+    return 0;
     }
-}
+
+
+    void build_lists(istream& in, List list_1, List list_2){
+        while(in >> x){
+            append(list_1,x,age?);
+            insert(list_2,x,age?);
+        }
+    }
