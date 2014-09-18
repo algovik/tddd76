9287ecbdc72b39d69f240b9c54913fcba1484648
Lisa <Lisa@c-77-178.eduroam.liu.se>
2014-09-18 10:28:41 +0200

    Starting lab2

---
+++ Lab2/Monetary.cc
@@ -0,0 +1,13 @@
+/**
+ * Filename: Monetary.cc
+ * Lab: Lab2 - Klass, operatoröverlagring, undantag
+ * Authors: Andreas Algovik     890718-0031 I5
+ *          Elisabeth Hanning   900419-2325 I5
+ * Date: 
+ * Description: 
+ */
+
+#include "Monetary.h"
+#include <iostream>
+
+using namespace std;
---
+++ Lab2/Monetary.h
@@ -0,0 +1,15 @@
+/**
+ * Filename: Monetary
+ * Lab: Lab2 - Klass, operatoröverlagring, undantag
+ * Authors: Andreas Algovik     890718-0031 I5
+ *          Elisabeth Hanning   900419-2325 I5
+ * Date: 
+ * Description:
+ */
+
+#ifndef MONETARY_H
+#define  MONETARY_H
+//#include <iostream>
+//#include <string>
+
+ #endif
